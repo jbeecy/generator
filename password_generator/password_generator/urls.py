b2a1c52c4066ac69b1a577c0f080456d68b9abbb
jbeecy <john.e.beecy@gmail.com>
2021-12-14 14:59:56 -0500

    remove 404 page on startup
    This commit changes the urls page so that when opening local host your are taken immediately to the generated password page
    @CaldoNic7

--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('generator/', include('generator.urls')),
+    path('', include('generator.urls')),
 ]
